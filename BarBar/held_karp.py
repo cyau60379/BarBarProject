6275f27c28df11fa8f499b84f538ea9891871063
cyau60379 <cyril.aubourg@isep.fr>
2020-06-12 23:39:21 +0200

    HK improvement + tests

--- BarBar/held_karp.py
+++ BarBar/held_karp.py
@@ -4,7 +4,7 @@ from Graph import Graph
 def held_karp(g, start):
     """
     Held-Karp algorithm for the Traveller Salesman Problem
-    :param g: a graph
+    :param Graph g: a graph
     :param start: the start node
     :return: the distance and the path to go to each node one time and return to start node
     """
@@ -21,23 +21,22 @@ def held_karp(g, start):
                 break
         if is_possible:
             s.remove(start)
-            distance, path = rec_held_karp(s, start, dist, [start])
+            distance, path = rec_held_karp(s, start, dist, [start], start)
             if not path:
                 return None
-            distance += dist[path[-1]][start]
-            path.append(start)
             return distance, path
         else:
             return None
 
 
-def rec_held_karp(s, current, dist, path):
+def rec_held_karp(s, current, dist, path, start):
     """
     Auxiliary recursive function for the Held-Karp algorithm
     :param list s: list of nodes of the graph that must be reached
     :param current: the current node
     :param dist: the distance matrix of all nodes
     :param path: the path from the start to the current node
+    :param start:
     :return: the tuple (distance, path) to go to the next node with the minimum cost
     """
     if len(s) == 1:
@@ -51,38 +50,18 @@ def rec_held_karp(s, current, dist, path):
                 continue
             new_s = s[:]
             new_s.remove(x)
-            distance, new_path = rec_held_karp(new_s, x, dist, path[:] + [x])
+            distance, new_path = rec_held_karp(new_s, x, dist, path[:] + [x], start)
             current_list.append((dist[current][x] + distance, new_path))
         if not current_list:
             return 999, []
-        return min(current_list)
+        if current == start:
+            return min(map(lambda tup: add_start_dist(current, dist, tup), current_list))
+        else:
+            return min(current_list)
 
 
-if __name__ == '__main__':
-    graph = Graph()
-
-    mat = [[999, 999, 1, 999, 2, 999],
-           [999, 999, 1, 999, 999, 999],
-           [1, 1, 999, 999, 999, 999],
-           [999, 999, 999, 999, 999, 1],
-           [2, 999, 999, 999, 999, 1],
-           [999, 999, 999, 1, 1, 999]]
-
-    for k in range(6):
-        graph.add_node(k)
-
-    for k in range(6):
-        for j in range(6):
-            if k != j:
-                graph.add_edge(k, j, mat[k][j])
-    print(held_karp(graph, 0))
-    print(graph.get_dist_matrix())
-
-
-"""
-    mat = [[999, 100, 1, 100, 2, 100],
-           [100, 999, 1, 2, 100, 100],
-           [1, 1, 999, 100, 100, 100],
-           [100, 2, 100, 999, 100, 1],
-           [2, 100, 100, 100, 999, 1],
-           [100, 100, 100, 1, 1, 999]]"""
+def add_start_dist(start, dist, tup):
+    new_dist = tup[0] + dist[tup[1][-1]][start]
+    final_path = tup[1]
+    final_path.append(start)
+    return new_dist, final_path
